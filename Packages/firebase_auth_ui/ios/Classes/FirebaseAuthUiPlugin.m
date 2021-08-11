fb64bd653644f6894534d5e8c182b2499b9ca6f9
Kota <kotarouv@gmail.com>
2021-08-12 02:10:09 +0900

    firebase_auth_uiを削除

--- Packages/firebase_auth_ui/ios/Classes/FirebaseAuthUiPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FirebaseAuthUiPlugin.h"
-#import <firebase_auth_ui/firebase_auth_ui-Swift.h>
-
-@implementation FirebaseAuthUiPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFirebaseAuthUiPlugin registerWithRegistrar:registrar];
-}
-@end
